fc6b4280072d2421c59da3317b2c0a949f0538a4
Anderson Gordillo <mauri201111@gmail.com>
2024-12-05 07:31:04 -0500

    02122024 refactor: Mejora en pagina web

--- index.php
+++ index.php
@@ -15,9 +15,80 @@
             <a href="index.php">
                 Shirt Shop
             </a>
-            <H1>HOLA SOY ANDERSON</H1>
         </div>
      </header>
+
+    <!-- MENU  -->
+
+    <nav id="menu">
+        <ul>
+            <li>
+                <a href="">
+                    INICIO 
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    CATEGORIA 
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    FIN 
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="content">
+        <!-- BARRA LATERAL  -->
+         <aside id="lateral">
+             <div id="login" class="block_aside">
+                <form action="#" method="post">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
+                    <input type="submit" value="enviar">
+                </form>
+                <a href="#">Mis Pedidos</a>
+                <a href="#">Gestionar Pedidos</a>
+                <a href="#">Gestionar Categorias</a>
+             </div>
+         </aside>
+
+         <div id="central">
+
+            <div class="product">
+                <img src="assets/img/camiseta.png" alt="">
+                <h2>Camiseta Azul</h2>
+                <p>30 Euros</p>
+                <a href="">Comprar</a>
+            </div>
+
+            <div class="product">
+                <img src="assets/img/camiseta.png" alt="">
+                <h2>Camiseta Azul</h2>
+                <p>30 Euros</p>
+                <a href="">Comprar</a>
+            </div>
+            <div class="product">
+                <img src="assets/img/camiseta.png" alt="">
+                <h2>Camiseta Azul</h2>
+                <p>30 Euros</p>
+                <a href="">Comprar</a>
+            </div>
+
+         </div>
+
+    </div>
+
+            <!-- PIE DE PAGINA  -->
+         <footer id="footer">
+                <p>
+                    Desarrollado por Anderson Gordillo &copy; <?=date('Y')?> 
+                </p>
+         </footer>    
 
 </body>
 </html>
